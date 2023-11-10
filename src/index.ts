eafa391764c23ef9a33f997f8efdf169dea39334
Andrei Dodu <andrei.dodu@hotmail.it>
2023-11-10 13:58:07 +0100

    chore: to be continue....

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log("Ciao");
+import "reflect-metadata";
+import {createConnection} from "typeorm";
+import * as express from "express";
+import {ApolloServer} from "apollo-server-express";
+
+import {typeDefs} from "./type/UserType";
+import {resolvers} from "./resolver/UserResolver";
+
+const startServer = async () => {
+    const server = new ApolloServer({typeDefs, resolvers});
+
+    await createConnection();
+
+    const app = express();
+
+    server.applyMiddleware({app});
+
+    app.listen({port: 4000}, () =>
+        console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+    );
+};
+
+startServer();
