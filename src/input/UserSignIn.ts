0e846b3192d4ecd4b1e5d42feb89fb69a6327f10
Andrei Dodu <andrei.dodu@hotmail.it>
2023-11-11 21:49:32 +0100

    fix: validator

--- src/input/UserSignIn.ts
+++ src/input/UserSignIn.ts
@@ -1,9 +1,13 @@
 import {Field, InputType} from "type-graphql";
+import {MinLength} from "class-validator";
 
 @InputType()
 export default class UserSignIn {
 
     @Field(() => String, {name: "email"})
+    @MinLength(5, {
+        message: 'email is too short',
+    })
     email: string;
 
     @Field(() => String, {name: "password"})
